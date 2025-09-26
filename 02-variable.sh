ab6872be0b1d97cf0bf4143e58e8b6a3911bb24e
kumar-cloud318 <kumar.cloud318@gmail.com>
2025-09-26 22:02:34 +0530

    varable.sh

---
+++ 02-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# No space before or after equal
+
+PERSON1="Sachin"
+PERSON2="Rahul"
+
+echo "$PERSON1: hi $PERSON2, how are you"
+echo "$PERSON2: am fine $PERSON1, how are you"
+echo "$PERSON1: what are you doing"
+echo "$PERSON2: am learning DevOps $PERSON1"
